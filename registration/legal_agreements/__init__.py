b9d95ea57ea8992f7538704c8542fc03f1009e7f
Alex Krantz <alex@krantz.dev>
2022-02-15 00:53:56 -0800

    Add creating legal agreements

--- registration/legal_agreements/__init__.py
+++ registration/legal_agreements/__init__.py
@@ -4,12 +4,17 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
-from common.database import LegalAgreement, with_db
+from common.database import (
+    LegalAgreement,
+    LegalAgreementCreate,
+    LegalAgreementRead,
+    with_db,
+)
 
 router = APIRouter()
 
 
-@router.get("/", response_model=List[LegalAgreement])
+@router.get("/", response_model=List[LegalAgreementRead])
 async def get_list(db: AsyncSession = Depends(with_db)):
     """
     Get a list of all legal agreements that must be acknowledged prior to submitting the participant's application.
@@ -17,3 +22,15 @@ async def get_list(db: AsyncSession = Depends(with_db)):
 
     result = await db.execute(select(LegalAgreement))
     return result.scalars().all()
+
+
+@router.post("/", response_model=LegalAgreementRead)
+async def create(agreement: LegalAgreementCreate, db: AsyncSession = Depends(with_db)):
+    """
+    Create a new legal agreement in the database
+    """
+    la = LegalAgreement.from_orm(agreement)
+    async with db.begin():
+        db.add(la)
+
+    return la
