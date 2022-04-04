c61d61d7a763a1906e1b996d74cb9f7c14a80049
Alex Krantz <alex@krantz.dev>
2022-04-03 18:42:33 -0700

    Expose authentication and permission dependencies

--- common/__init__.py
+++ common/__init__.py
@@ -1,3 +1,5 @@
+from .authentication import is_authenticated, with_user_id
 from .database import with_db
 from .kv import NamespacedClient, with_kv
+from .permissions import Permission, requires_permissions
 from .settings import SETTINGS
