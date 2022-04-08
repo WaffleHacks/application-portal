1c1ed7e609bf1dd8c928d5a3eea1b823a66bc461
Alex Krantz <alex@krantz.dev>
2022-04-08 01:16:04 -0700

    Revert "Switch to react 18 rendering" pending tailwindlabs/headlessui#681
    This reverts commit d7eac8e74922c40fdaa1a6393282e7c963752be5.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
 import Layout from './components/Layout';
 
-const root = document.getElementById('root') as Element;
-createRoot(root).render(
+ReactDOM.render(
   <React.StrictMode>
     <Layout>
       <App />
     </Layout>
   </React.StrictMode>,
+  document.getElementById('root'),
 );
