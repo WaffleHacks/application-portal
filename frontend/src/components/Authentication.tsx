71d59c748596d9d0da3e7cde6f9923e7100e5a04
Alex Krantz <alex@krantz.dev>
2022-04-14 12:54:33 -0700

    Add token fetching

--- frontend/src/components/Authentication.tsx
+++ frontend/src/components/Authentication.tsx
@@ -1,38 +1,56 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { RefreshIcon } from '@heroicons/react/outline';
-import classNames from 'classnames';
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 
+import { useSetTokens } from '../tokens';
 import Layout from './Layout';
+
+// From https://github.com/auth0/auth0-react/blob/88f82318a1dbe1372dd1653aec5bd609ccd8a301/src/utils.tsx#L3-L9
+const CODE_RE = /[?&]code=[^&]+/;
+const STATE_RE = /[?&]state=[^&]/;
+const ERROR_RE = /[?&]error=[^&]/;
+
+const hasAuthParams = (searchParams = window.location.search): boolean =>
+  (CODE_RE.test(searchParams) || ERROR_RE.test(searchParams)) && STATE_RE.test(searchParams);
 
 interface Props {
   children: ReactNode;
 }
 
 const Authentication = ({ children }: Props): JSX.Element => {
-  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
-  const [loginTimer, setLoginTimer] = useState<NodeJS.Timeout>();
+  const { isAuthenticated, isLoading, loginWithRedirect, getAccessTokenSilently } = useAuth0();
+  const { profileToken, portalToken, setProfileToken, setPortalToken } = useSetTokens();
 
-  const loading = isLoading || !isAuthenticated;
-
-  // TODO: figure out a better way to require login w/o user interaction
   useEffect(() => {
-    if (isLoading) return;
+    (async () => {
+      if (isLoading || hasAuthParams()) return;
+      else if (!isAuthenticated) await loginWithRedirect();
 
-    if (isAuthenticated && loginTimer) clearTimeout(loginTimer);
-    else if (!loginTimer) setLoginTimer(setTimeout(() => loginWithRedirect(), 1000));
-  }, [isLoading, isAuthenticated, loginTimer]);
+      // Get tokens for the application portal and profile services
+      const [resolvedPortal, resolvedProfile] = await Promise.all([
+        getAccessTokenSilently({
+          audience: 'https://apply.wafflehacks.tech',
+        }),
+        getAccessTokenSilently({
+          audience: 'https://id.wafflehacks.org',
+        }),
+      ]);
+      setPortalToken(resolvedPortal);
+      setProfileToken(resolvedProfile);
+    })();
+  }, [isLoading, isAuthenticated]);
 
-  return (
-    <>
-      <div className={classNames({ hidden: !loading }, 'h-screen flex')}>
+  if (isLoading || !isAuthenticated || portalToken === '' || profileToken === '') {
+    return (
+      <div className="h-screen flex">
         <div className="m-auto">
           <RefreshIcon className="w-16 h-16 rounded-full animate-spin" />
         </div>
       </div>
-      <Layout className={loading ? 'hidden' : ''}>{children}</Layout>
-    </>
-  );
+    );
+  }
+
+  return <Layout>{children}</Layout>;
 };
 
 export default Authentication;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,33 +1,35 @@
 import { Auth0Provider } from '@auth0/auth0-react';
+import { Provider } from 'jotai';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
+
 import Authentication from './components/Authentication';
 import Placeholder from './Placeholder';
-import Scope from './scopes';
 
 const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || '';
 const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={AUTH0_DOMAIN}
-      clientId={AUTH0_CLIENT_ID}
-      redirectUri={window.location.origin}
-      cacheLocation="localstorage"
-      audience="https://apply.wafflehacks.tech"
-      scope={Object.values(Scope).join(' ')}
-      useRefreshTokens
-    >
-      <BrowserRouter>
-        <Authentication>
-          <Placeholder />
-        </Authentication>
-      </BrowserRouter>
-    </Auth0Provider>
+    <Provider>
+      <Auth0Provider
+        domain={AUTH0_DOMAIN}
+        clientId={AUTH0_CLIENT_ID}
+        redirectUri={window.location.origin}
+        cacheLocation="localstorage"
+        audience="https://apply.wafflehacks.tech"
+        useRefreshTokens
+      >
+        <BrowserRouter>
+          <Authentication>
+            <Placeholder />
+          </Authentication>
+        </BrowserRouter>
+      </Auth0Provider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
