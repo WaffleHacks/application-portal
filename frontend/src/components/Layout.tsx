2a98b1e9839a85265410ccac7bf960e0d116db3f
Alex Krantz <alex@krantz.dev>
2022-04-15 23:32:23 -0700

    Update page title format

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -26,7 +26,7 @@ const Layout = ({ className, children }: Props): JSX.Element => {
       <div className="py-10">
         <header>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold leading-tight text-gray-900">{title}</h1>
+            <h1 className="text-3xl font-bold leading-tight text-gray-900">Your {title}</h1>
           </div>
         </header>
         <main className="pt-3">
