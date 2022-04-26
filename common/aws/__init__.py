6f0cd98dd614d2916d928f12522d70edd03fb774
Alex Krantz <alex@krantz.dev>
2022-04-25 23:26:34 -0700

    Add minimal typing stubs for runtime

--- common/aws/__init__.py
+++ common/aws/__init__.py
@@ -4,9 +4,11 @@ import boto3
 
 if TYPE_CHECKING:
     from mypy_boto3_s3 import S3Client
+else:
+    from .stubs import S3Client
 
 
-def with_s3() -> "S3Client":
+def with_s3() -> S3Client:
     """
     Get an S3 client
     """
---
+++ common/aws/stubs.py
@@ -0,0 +1,18 @@
+from typing import Any, Dict, List
+
+from botocore.client import BaseClient
+
+# All classes in this file should not be used directly
+# They are strictly for type-checking purposes
+
+
+class S3Client(BaseClient):
+    def generate_presigned_post(
+        self,
+        Bucket: str,
+        Key: str,
+        Fields: Dict[str, Any],
+        Conditions: List[Any],
+        ExpiresIn: int = 3600,
+    ) -> Dict[str, Any]:
+        pass
