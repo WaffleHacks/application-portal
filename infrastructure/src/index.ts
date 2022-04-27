b5190d0f05d18052c53894cd30fbb52f3460e5ba
Alex Krantz <alex@krantz.dev>
2022-04-26 17:58:27 -0700

    Add sync resources

--- infrastructure/src/index.ts
+++ infrastructure/src/index.ts
@@ -1,11 +1,23 @@
-import { ComponentResource, ComponentResourceOptions, ResourceOptions } from '@pulumi/pulumi';
+import { ComponentResource, ComponentResourceOptions, Output, ResourceOptions } from '@pulumi/pulumi';
+
+import Sync from './sync';
+
+interface Args {
+  syncTopic: string;
+}
 
 class ApplicationPortal extends ComponentResource {
-  constructor(name: string, opts?: ComponentResourceOptions) {
+  public readonly policies: Output<string>[];
+
+  constructor(name: string, args: Args, opts?: ComponentResourceOptions) {
     super('wafflehacks:application-portal:ApplicationPortal', name, { options: opts }, opts);
 
     const defaultResourceOptions: ResourceOptions = { parent: this };
+    const { syncTopic } = args;
 
+    const sync = new Sync(`${name}-sync`, { topic: syncTopic }, defaultResourceOptions);
+
+    this.policies = [sync.policy];
     this.registerOutputs();
   }
 }
