08bc0deeece38251dfdc9e15d72d8272ae8c3542
Alex Krantz <alex@krantz.dev>
2022-05-14 15:29:26 -0700

    Fix client session exceptions

--- common/mail/mjml.py
+++ common/mail/mjml.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import ujson
 from aiohttp import ClientSession
 
@@ -5,7 +7,14 @@ from aiohttp import ClientSession
 class MJMLClient(object):
     def __init__(self, base_url: str):
         self.url = base_url + "/render"
-        self.session = ClientSession(json_serialize=ujson.dumps)
+        self.session: Optional[ClientSession] = None
+
+    async def __init_session(self):
+        """
+        Initialize the client session if it does not exist
+        """
+        if self.session is None:
+            self.session = ClientSession(json_serialize=ujson.dumps)
 
     async def render(self, source: str) -> str:
         """
@@ -13,6 +22,9 @@ class MJMLClient(object):
         :param source: the input MJML
         :returns: HTML
         """
+        await self.__init_session()
+        assert self.session is not None
+
         resp = await self.session.post(self.url, json={"mjml": source})
         resp.raise_for_status()
 
