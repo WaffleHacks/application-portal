56489029f7646f9148b847a08992cb2ef463c3ee
Alex Krantz <alex@krantz.dev>
2022-05-14 17:49:28 -0700

    Add code splitting for permission based-views

--- frontend/src/organizers/index.tsx
+++ frontend/src/organizers/index.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import NotFound from '../pages/NotFound';
 import Layout from './components/Layout';
-import { ApplicationDetail, ListApplications } from './pages/applications';
-import { MessageDetail, MessageEdit, MessageNew, MessagesList } from './pages/messages';
-import { SchoolDetail, SchoolList } from './pages/schools';
+
+const ApplicationDetail = React.lazy(() => import('./pages/applications/Detail'));
+const ListApplications = React.lazy(() => import('./pages/applications/List'));
+const MessageDetail = React.lazy(() => import('./pages/messages/Detail'));
+const MessageEdit = React.lazy(() => import('./pages/messages/Edit'));
+const MessageNew = React.lazy(() => import('./pages/messages/New'));
+const MessagesList = React.lazy(() => import('./pages/messages/List'));
+const SchoolDetail = React.lazy(() => import('./pages/schools/Detail'));
+const SchoolList = React.lazy(() => import('./pages/schools/List'));
 
 const Organizers = (): JSX.Element => (
   <Layout>
-    <Routes>
-      <Route path="/applications" element={<ListApplications />} />
-      <Route path="/applications/:id" element={<ApplicationDetail />} />
-      <Route path="/schools" element={<SchoolList />} />
-      <Route path="/schools/:id" element={<SchoolDetail />} />
-      <Route path="/messages" element={<MessagesList />} />
-      <Route path="/messages/new" element={<MessageNew />} />
-      <Route path="/messages/:id" element={<MessageDetail />} />
-      <Route path="/messages/:id/edit" element={<MessageEdit />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<>Loading...</>}>
+      <Routes>
+        <Route path="/applications" element={<ListApplications />} />
+        <Route path="/applications/:id" element={<ApplicationDetail />} />
+
+        <Route path="/schools" element={<SchoolList />} />
+        <Route path="/schools/:id" element={<SchoolDetail />} />
+
+        <Route path="/messages" element={<MessagesList />} />
+        <Route path="/messages/new" element={<MessageNew />} />
+        <Route path="/messages/:id" element={<MessageDetail />} />
+        <Route path="/messages/:id/edit" element={<MessageEdit />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   </Layout>
 );
 
--- frontend/src/participants/index.tsx
+++ frontend/src/participants/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import NotFound from '../pages/NotFound';
 import Layout from './components/Layout';
-import { Application } from './pages/application';
-import Status from './pages/Status';
+
+const Application = React.lazy(() => import('./pages/application/Application'));
+const Status = React.lazy(() => import('./pages/Status'));
 
 const Participants = (): JSX.Element => (
   <Layout>
-    <Routes>
-      <Route index element={<Status />} />
-      <Route path="/new" element={<Application />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<>Loading...</>}>
+      <Routes>
+        <Route index element={<Status />} />
+        <Route path="/new" element={<Application />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   </Layout>
 );
 
