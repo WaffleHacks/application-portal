00b9deb728fea83048a2999775d217b93f790aca
Alex Krantz <alex@krantz.dev>
2022-05-22 17:09:43 -0700

    Remove unused imports

--- integrations/main.py
+++ integrations/main.py
@@ -1,6 +1,3 @@
-from http import HTTPStatus
-
-from authlib.integrations.base_client.errors import MismatchingStateError
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import UJSONResponse
