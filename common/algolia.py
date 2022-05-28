a9d7ed66729cce63453f8d1e530383826b1bc2f5
Alex Krantz <alex@krantz.dev>
2022-05-28 13:30:02 -0700

    Add algolia client dependency

---
+++ common/algolia.py
@@ -0,0 +1,26 @@
+from algoliasearch.configs import SearchConfig
+from algoliasearch.http.requester_async import RequesterAsync
+from algoliasearch.http.transporter_async import TransporterAsync
+from algoliasearch.search_client import SearchClient
+from algoliasearch.search_client_async import SearchClientAsync, SearchIndexAsync
+
+from common import SETTINGS
+
+_config = SearchConfig(
+    SETTINGS.registration.algolia_app_id,
+    SETTINGS.registration.algolia_api_key,
+)
+_sync_client = SearchClient.create_with_config(_config)
+_client = SearchClientAsync(
+    _sync_client,
+    TransporterAsync(RequesterAsync(), _config),
+    _config,
+)
+
+
+async def with_schools_index() -> SearchIndexAsync:
+    """
+    Get an Algolia search client for the schools index
+    :return:
+    """
+    return _client.init_index("schools")
