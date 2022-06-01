bbf4d90e633200ff847d7bbbc1ee8dfc4ec9f125
Alex Krantz <alex@krantz.dev>
2022-06-01 15:59:51 -0700

    Merge attendance and feedback routes

--- workshops/attendance.py
+++ workshops/attendance.py
@@ -1,7 +1,8 @@
 from http import HTTPStatus
 from typing import Optional
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
+from pydantic import BaseModel
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -10,6 +11,8 @@ from common.authentication import with_user_id
 from common.database import (
     Event,
     EventAttendance,
+    Feedback,
+    FeedbackCreate,
     require_application_accepted,
     with_db,
 )
@@ -32,6 +35,83 @@ async def mark(
     """
     Mark the participant as having attended the event by its code
     """
+    event = await get_event_by_code(code, db)
+
+    try:
+        attendance = EventAttendance(event_id=event.id, participant_id=user_id)
+
+        db.add(attendance)
+        await db.commit()
+    except IntegrityError:
+        # Already marked their attendance, all good
+        pass
+
+
+class StatusResponse(BaseModel):
+    submitted: bool
+
+
+@router.get(
+    "/{code}/feedback",
+    name="Check feedback submitted",
+    response_model=StatusResponse,
+)
+async def status(
+    code: str,
+    user_id: str = Depends(with_user_id),
+    db: AsyncSession = Depends(with_db),
+):
+    """
+    Check if the requester has already submitted feedback for the specified event
+    """
+    statement = (
+        select(Feedback)
+        .join(Event)
+        .where(Event.code == code)
+        .where(Feedback.participant_id == user_id)
+    )
+    result = await db.execute(statement)
+    feedback = result.scalars().first()
+
+    return {"submitted": feedback is not None}
+
+
+@router.put(
+    "/{code}/feedback",
+    name="Submit feedback",
+    status_code=HTTPStatus.CREATED,
+    response_class=Response,
+)
+async def submit(
+    code: str,
+    params: FeedbackCreate,
+    user_id: str = Depends(with_user_id),
+    db: AsyncSession = Depends(with_db),
+):
+    """
+    Submit feedback for an event
+    """
+    event = await get_event_by_code(code, db)
+
+    try:
+        feedback = Feedback.from_orm(
+            params,
+            {"event_id": event.id, "participant_id": user_id},
+        )
+
+        db.add(feedback)
+        await db.commit()
+    except IntegrityError:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT,
+            detail="already submitted feedback",
+        )
+
+
+async def get_event_by_code(code: str, db: AsyncSession) -> Event:
+    """
+    Get an event by its code
+    """
     statement = select(Event).where(Event.code == code)
     result = await db.execute(statement)
     event: Optional[Event] = result.scalars().first()
@@ -42,11 +122,4 @@ async def mark(
             detail="invalid attendance code",
         )
 
-    try:
-        attendance = EventAttendance(event_id=event.id, participant_id=user_id)
-
-        db.add(attendance)
-        await db.commit()
-    except IntegrityError:
-        # Already marked their attendance, all good
-        pass
+    return event
