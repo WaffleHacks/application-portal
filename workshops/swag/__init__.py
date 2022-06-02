f49e06443fc4e4b98e0a9b2bdeb2172edb96d131
Alex Krantz <alex@krantz.dev>
2022-06-01 18:25:50 -0700

    Document swag tier progress response

--- workshops/swag/__init__.py
+++ workshops/swag/__init__.py
@@ -2,10 +2,12 @@ from typing import List, Optional
 
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from common.database import (
+    EventAttendance,
     Participant,
     SwagTier,
     SwagTierListWithDescription,
@@ -23,6 +25,7 @@ router.include_router(tiers.router, prefix="/tiers")
 
 
 class SwagStatus(BaseModel):
+    attended: int
     current_tier: Optional[int]
     tiers: List[SwagTierListWithDescription]
 
@@ -30,6 +33,7 @@ class SwagStatus(BaseModel):
 @router.get(
     "/progress",
     name="Get current progress",
+    response_model=SwagStatus,
     dependencies=[
         Depends(requires_permission(Permission.Participant)),
         Depends(require_application_accepted),
@@ -43,7 +47,20 @@ async def progress(
     Get the current user's progress through the swag tiers
     """
     # Get all the tiers
-    result = await db.execute(select(SwagTier).order_by(SwagTier.required_attendance))
+    result = await db.execute(
+        select(SwagTier).order_by(SwagTier.required_attendance.desc())  # type: ignore
+    )
     swag_tiers = result.scalars().all()
 
-    return {"current_tier": participant.swag_tier, "tiers": swag_tiers}
+    result = await db.execute(
+        select(func.count())
+        .select_from(EventAttendance)
+        .where(EventAttendance.participant_id == participant.id)
+    )
+    attended_events = result.scalar()
+
+    return {
+        "attended": attended_events,
+        "current_tier": participant.swag_tier_id,
+        "tiers": swag_tiers,
+    }
