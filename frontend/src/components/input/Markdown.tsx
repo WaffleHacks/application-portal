74fb061d3be1e9cb891cfd806ec28a5709560b13
Alex Krantz <alex@krantz.dev>
2022-06-03 09:24:21 -0700

    Add link to gfm

--- frontend/src/components/input/Markdown.tsx
+++ frontend/src/components/input/Markdown.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
+import Link from '../Link';
 import RenderMarkdown from '../RenderMarkdown';
 import { BaseProps, generateId } from './common';
 
@@ -83,8 +84,14 @@ const Markdown = ({ label, ...props }: BaseProps<string>): JSX.Element => {
       </Tab.Group>
       <div className="mt-2 mx-3 flex justify-between text-sm">
         <p className="text-gray-500">
-          You can use GitHub-flavored markdown here to format the text. Please only use headings if absolutely
-          necessary.
+          You can use{' '}
+          <Link
+            to="https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax"
+            external
+          >
+            GitHub-flavored markdown
+          </Link>{' '}
+          here to format the text. Please only use headings if absolutely necessary.
         </p>
         {error && (
           <p className="text-red-600" id={errorId}>
