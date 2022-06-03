8de5ee80997e349824744882a88afa3f84985a2f
Alex Krantz <alex@krantz.dev>
2022-06-03 09:55:14 -0700

    Allow progress and description columns to have independent heights

--- frontend/src/participants/pages/swag-progress/SwagProgress.tsx
+++ frontend/src/participants/pages/swag-progress/SwagProgress.tsx
@@ -28,22 +28,26 @@ const SwagProgress = (): JSX.Element => {
 
   return (
     <div className="grid grid-cols-1 gap-6 lg:grid-flow-col-dense lg:grid-cols-3">
-      <Card>
-        <ProgressBar
-          steps={data.tiers.map((t) => ({ title: t.name, required: t.required_attendance, id: t.id }))}
-          progress={data.attended}
-          selected={selected}
-          onSelect={setSelected}
-        />
-        <p className="mt-3 text-sm text-gray-400">
-          <ArrowUpIcon className="h-4 w-4 ml-2 my-1" />
-          Click on each tier to see their rewards!
-        </p>
-      </Card>
-      <Card className="lg:col-span-2">
-        <h1 className="text-3xl font-bold">{current.name}</h1>
-        <RenderMarkdown content={current.description} />
-      </Card>
+      <div className="lg:col-start-1">
+        <Card>
+          <ProgressBar
+            steps={data.tiers.map((t) => ({ title: t.name, required: t.required_attendance, id: t.id }))}
+            progress={data.attended}
+            selected={selected}
+            onSelect={setSelected}
+          />
+          <p className="mt-3 text-sm text-gray-400">
+            <ArrowUpIcon className="h-4 w-4 ml-2 my-1" />
+            Click on each tier to see their rewards!
+          </p>
+        </Card>
+      </div>
+      <div className="lg:col-start-2 lg:col-span-2">
+        <Card>
+          <h1 className="text-3xl font-bold">{current.name}</h1>
+          <RenderMarkdown content={current.description} />
+        </Card>
+      </div>
     </div>
   );
 };
