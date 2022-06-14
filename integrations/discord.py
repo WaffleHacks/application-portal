b9cda6bb29ca2d4263198777d837f1c726c02960
Alex Krantz <alex@krantz.dev>
2022-06-14 19:16:49 +0200

    Allow getting application status by email

--- integrations/discord.py
+++ integrations/discord.py
@@ -1,12 +1,43 @@
+from typing import Optional
+
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
 
 from common.authentication import is_internal
 from common.database import ApplicationStatus, Participant, with_db
 
 router = APIRouter()
+
+
+class StatusResponse(BaseModel):
+    status: Optional[ApplicationStatus]
+
+
+@router.get(
+    "/status",
+    response_model=StatusResponse,
+    dependencies=[Depends(is_internal)],
+    name="Get application status",
+)
+async def status(email: str, db: AsyncSession = Depends(with_db)):
+    """
+    Get the current status of a participant's application.
+    """
+    statement = (
+        select(Participant)
+        .where(Participant.email == email)
+        .options(selectinload(Participant.application))
+    )
+    result = await db.execute(statement)
+    participant: Optional[Participant] = result.scalar()
+
+    if participant is None or participant.application is None:
+        return {"status": None}
+    else:
+        return {"status": participant.application.status}
 
 
 class CanLinkResponse(BaseModel):
