1b4bea279e7b9172c3fad9c3d1faf000a6651a16
Alex Krantz <alex@krantz.dev>
2023-05-14 14:16:31 -0700

    Widen default organizer layout

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        layout: '96rem',
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
