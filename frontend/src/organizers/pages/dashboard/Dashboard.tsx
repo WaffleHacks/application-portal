eb2208093c111e9eb97f5dba9975544a3e9ad3cd
Alex Krantz <alex@krantz.dev>
2023-05-31 11:51:40 -0700

    Add bar charts

--- frontend/src/organizers/pages/dashboard/Dashboard.tsx
+++ frontend/src/organizers/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import BarChart from './BarChart';
+import ChartContainer from './ChartContainer';
 import PieChart from './PieChart';
 
 const Dashboard = (): JSX.Element => {
@@ -14,18 +16,19 @@ const Dashboard = (): JSX.Element => {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center mt-6 gap-y-8 gap-x-4">
-        <PieChart title="Gender" source="gender" />
-        <PieChart title="Race / Ethnicity" source="race-ethnicity" />
-        <PieChart title="Past Hackathon Experience" source="experience" />
-        <PieChart title="Level of Study" source="level-of-study" />
+        <ChartContainer title="Gender" source="gender" chart={PieChart} />
+        <ChartContainer title="Race / Ethnicity" source="race-ethnicity" chart={PieChart} />
+        <ChartContainer title="Past Hackathon Experience" source="experience" chart={PieChart} />
+        <ChartContainer title="Level of Study" source="level-of-study" chart={PieChart} />
       </div>
+
+      <ChartContainer className="mt-2" title="Age" source="age" chart={BarChart} width={1600} />
+      <ChartContainer className="mt-2" title="Graduation Year" source="graduation-year" chart={BarChart} width={1600} />
 
       {/*
         TODO:
           - per day as line graph w/ range selector
           - countries as choropleth map
-          - age as bar chart
-          - graduation year as bar chart
           - school as ?
           - major as ?
         */}
--- frontend/src/organizers/pages/dashboard/PieChart.tsx
+++ frontend/src/organizers/pages/dashboard/PieChart.tsx
@@ -1,5 +1,3 @@
-import { ArrowPathIcon, QuestionMarkCircleIcon } from '@heroicons/react/24/outline';
-import classNames from 'classnames';
 import {
   PieArcDatum,
   arc,
@@ -12,67 +10,21 @@ import {
   schemeSpectral,
   select,
 } from 'd3';
-import React, { ComponentType, useEffect, useId, useRef } from 'react';
+import React, { useEffect, useId, useRef } from 'react';
 
-import { RegistrationStatistic, StatisticEntry, useGetRegistrationStatisticsQuery } from 'store';
+import { ChartProps } from './ChartContainer';
 
 const HEIGHT = 400;
 
-interface ContainerProps {
-  title: string;
-  source: RegistrationStatistic;
-  width?: number;
-}
-
-const Container = ({ source, title, width = 400 }: ContainerProps): JSX.Element => {
-  const { data = [], isLoading } = useGetRegistrationStatisticsQuery(source);
-
-  let component;
-
-  // TODO: add empty states
-  if (isLoading) component = <IconWithLabel text="Loading..." className="animate-spin" icon={ArrowPathIcon} />;
-  else if (data.length === 0) component = <IconWithLabel text="No data yet" icon={QuestionMarkCircleIcon} />;
-  else component = <Chart data={data} width={width} />;
-
-  return (
-    <div>
-      <p className="w-full text-center mt-1 mb-3 text-xl text-gray-800">{title}</p>
-      {component}
-    </div>
-  );
-};
-
-interface IconProps {
-  className?: string;
-  'aria-hidden'?: boolean | 'true' | 'false';
-}
-
-interface IconWithLabelProps {
-  className?: string;
-  text: string;
-  icon: ComponentType<IconProps>;
-}
-
-const IconWithLabel = ({ className, text, icon: Icon }: IconWithLabelProps): JSX.Element => (
-  <>
-    <div className="flex justify-center">
-      <Icon className={classNames(className, 'w-16 h-16')} aria-hidden="true" />
-    </div>
-    <p className="text-center text-md text-gray-500">{text}</p>
-  </>
-);
-
-interface ChartProps {
-  data: StatisticEntry[];
-  width: number;
-}
-
-const Chart = ({ data, width }: ChartProps): JSX.Element => {
+const PieChart = ({ data, width }: ChartProps): JSX.Element => {
   const id = useId();
   const ref = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
     if (ref.current === null) return;
+
+    // Clear the current SVG before re-drawing
+    ref.current.replaceChildren();
 
     const radius = Math.min(width, HEIGHT) / 2;
     const labelRadius = radius * 0.75;
@@ -153,4 +105,4 @@ const generateColorScheme = (size: number): readonly string[] => {
   return quantize((t) => interpolateSpectral(t * 0.8 + 0.1), size);
 };
 
-export default Container;
+export default PieChart;
