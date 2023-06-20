6326ad1a59bba36d8e805716641de1e277d1fc41
Alex Krantz <alex@krantz.dev>
2023-06-20 08:28:52 -0700

    Add empty state for swag tiers when none are configured

--- frontend/src/participants/pages/swag-progress/SwagProgress.tsx
+++ frontend/src/participants/pages/swag-progress/SwagProgress.tsx
@@ -1,4 +1,4 @@
-import { ArrowUpIcon } from '@heroicons/react/24/outline';
+import { ArrowUpIcon, DocumentIcon } from '@heroicons/react/24/outline';
 import React, { useEffect, useState } from 'react';
 
 import Card from 'components/Card';
@@ -8,15 +8,28 @@ import { useGetSwagProgressQuery } from 'store';
 
 import ProgressBar from './ProgressBar';
 
+const NoTiersConfigured = (): JSX.Element => (
+  <Card className="text-center py-5">
+    <DocumentIcon className="mx-auto h-12 w-12 text-gray-400" />
+    <h3 className="mt-2 text-md font-medium text-gray-900">
+      No swag tiers have been setup just yet. Check back later!
+    </h3>
+  </Card>
+);
+
 const SwagProgress = (): JSX.Element => {
   const { data, isLoading } = useGetSwagProgressQuery();
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState<number>();
 
   useEffect(() => {
-    if (!isLoading && data !== undefined) setSelected(data.current_tier || data.tiers[data.tiers.length - 1].id);
+    if (isLoading || data === undefined) return;
+    if (data.tiers.length === 0) return;
+
+    setSelected(data.current_tier || data.tiers[data.tiers.length - 1].id);
   }, [isLoading, data]);
 
   if (isLoading || data === undefined) return <Loading />;
+  if (selected === undefined) return <NoTiersConfigured />;
 
   const current = data.tiers.find((t) => t.id === selected) || data.tiers[data.tiers.length - 1];
 
